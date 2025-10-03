eb991888238a346e554abb1e79fedcf5e64b798d
What-XD <101012824+What-XD@users.noreply.github.com>
2025-10-04 00:31:57 +0500

    cors

--- src/main.go
+++ src/main.go
@@ -6,20 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"orencode/src/routers"
 	"github.com/joho/godotenv"
+	"github.com/gin-contrib/cors"
 )
 
+
 func main() {
-	godotenv.Load()
+    godotenv.Load()
 
-	r := gin.Default()
+    r := gin.Default()
 
-	routers.ValidationKeycloak(r)
-	// authMiddleware := 
-	routers.CreateReq(r)
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        AllowCredentials: true,
+    }))
 
-	log.Println("Server started at :8080")
-	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
-		log.Fatal(err)
-	}
+    routers.ValidationKeycloak(r)
+    routers.CreateReq(r)
 
+    log.Println("Server started at :8080")
+    if err := r.Run(":" + os.Getenv("PORT")); err != nil {
+        log.Fatal(err)
+    }
 }
