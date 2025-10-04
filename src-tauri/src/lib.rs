78ef0dd8344ffd7c0b91eb20c53ce6e0e0a590e2
GoidaZ <blyatskiy.suka@gmail.com>
2025-10-04 14:44:44 +0500

    a

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,7 +23,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::default().build())
         .setup(|app| {
             let store = app.store("settings.json")?;
-            store.set("api_base", json!("http://localhost:8000"));
+            store.set("api_base", json!("http://localhost:5859"));
             store.save()?;
             Ok(())
         })
