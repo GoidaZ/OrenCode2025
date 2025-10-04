d9eab4d9cb074c179ab6383d7453d8eb67a5862b
What-XD <101012824+What-XD@users.noreply.github.com>
2025-10-04 16:24:13 +0500

    delete react-query-devtools (Мне лень вырубать его)

--- src/util/query.provider.tsx
+++ src/util/query.provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useState } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
@@ -9,7 +9,7 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false}/>
+            {/* <ReactQueryDevtools initialIsOpen={false}/> */}
             {children}
         </QueryClientProvider>
     )
