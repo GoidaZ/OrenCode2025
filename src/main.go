e287a204d9a3433ca910d5538a12a0560b39c62e
GoidaZ <blyatskiy.suka@gmail.com>
2025-10-05 14:43:46 +0500

    a

--- src/main.go
+++ src/main.go
@@ -51,7 +51,7 @@ func main() {
 
 	slog.Debug("Configuring CORS middleware")
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3001", "tauri://"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
